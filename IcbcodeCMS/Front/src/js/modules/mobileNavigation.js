e2f58b6ae2262274922827f0dd20f1be93b064e1
dbarsukov <dbarsukov@optimalgroup.ru>
2024-09-16 11:54:15 +0300

    Changed socials

--- IcbcodeCMS/Front/src/js/modules/mobileNavigation.js
+++ IcbcodeCMS/Front/src/js/modules/mobileNavigation.js
@@ -1,13 +1,15 @@
 export default function() {
-  let nav = document.querySelector(".header__nav");
-  let headerButtons = document.querySelector(".header__buttons");
-  if (nav) {
-    let navCloned = nav.cloneNode(true);
-    //let headerButtonsCloned = headerButtons.cloneNode(true);
+	let nav = document.querySelector('.header__nav')
+	let socials = document.querySelector('.header .follow-header')
+	let headerButtons = document.querySelector('.header__buttons')
+	if (nav) {
+		let navCloned = nav.cloneNode(true)
+		let socialsCloned = socials.cloneNode(true)
+		//let headerButtonsCloned = headerButtons.cloneNode(true);
 
-    document.querySelector(".header__row").insertAdjacentHTML(
-      "beforeend",
-      `
+		document.querySelector('.header__row').insertAdjacentHTML(
+			'beforeend',
+			`
         <button class="nav-button">
             <div class="hamburger">
                 <span class="line"></span>
@@ -15,26 +17,26 @@ export default function() {
                 <span class="line"></span>
             </div>
         </button>`
-    );
+		)
 
-    let navBtn = document.querySelector(".header .nav-button");
-    let mobileMenu = document.createElement("div");
-    mobileMenu.classList.add("mobile-menu");
-    let menuContainer = document.createElement("div");
-    menuContainer.classList.add("container");
-    mobileMenu.appendChild(menuContainer);
-    //menuContainer.appendChild(headerButtonsCloned);
-    menuContainer.appendChild(navCloned);
+		let navBtn = document.querySelector('.header .nav-button')
+		let mobileMenu = document.createElement('div')
+		mobileMenu.classList.add('mobile-menu')
+		let menuContainer = document.createElement('div')
+		menuContainer.classList.add('container')
+		mobileMenu.appendChild(menuContainer)
+		//menuContainer.appendChild(headerButtonsCloned);
+		menuContainer.appendChild(navCloned)
+		menuContainer.appendChild(socialsCloned)
 
-    // Append menu
-    document.body.appendChild(mobileMenu);
+		// Append menu
+		document.body.appendChild(mobileMenu)
 
-    navBtn.addEventListener("click", function(e) {
-      document
-        .querySelector(".header .hamburger")
-        .classList.toggle("is-active");
-      mobileMenu.classList.toggle("open");
-      document.body.classList.toggle("overflow-transparent");
-    });
-  }
+		navBtn.addEventListener('click', function(e) {
+			document.querySelector('.header .hamburger').classList.toggle('is-active')
+			mobileMenu.classList.toggle('open')
+			document.body.classList.toggle('overflow-transparent')
+		})
+	}
 }
+
